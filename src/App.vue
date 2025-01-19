a319288087801db2f2bd9febd3d23576883f72ba
AntoineDly <antoine.delaunay333@gmail.com>
2025-01-18 18:24:48 -0800

    use axios to consume api

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <button @click="login">Connexion</button>
+  <button @click="character">Character</button>
+  <p>Page principal {{ response }}</p>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+<script setup lang="ts">
+import axios from "axios";
+import { ref } from "vue";
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
-</script>
+const response = ref("");
+const config = ref();
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+async function login() {
+  const response = await axios.post("http://127.0.0.1:8000/api/login", {
+    email: "myster.gryffus@gmail.com",
+    password: "test123",
+  });
+  const token = response.data.data.token;
+  config.value = {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  };
 }
-</style>
+
+async function character() {
+  const data = await axios.get(
+    "http://127.0.0.1:8000/api/characters/a5011d8a-de24-4e8c-91cb-91edaa8b6fa7/with_linked_items",
+    config.value
+  );
+  response.value = data.data.data;
+}
+</script>
