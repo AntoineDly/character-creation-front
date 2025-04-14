12537aa3236dcacf1fd0ba04b5f33a6f567512b7
AntoineDly <antoine.delaunay333@gmail.com>
2025-04-14 16:15:01 -0700

    begin of using proper import in order to save performance, need to do it everywhere

--- src/pages/Component/ComponentField/routes.ts
+++ src/pages/Component/ComponentField/routes.ts
@@ -1,11 +1,10 @@
 import { RouteRecordRaw } from 'vue-router'
-import CreateComponentFieldPage from './CreateComponentFieldPage.vue'
 import { RouteNameComponentFieldEnum } from '@/router/router.enum'
 
 export const componentFieldRoutes: RouteRecordRaw[] = [
   {
     path: '/components/:componentId/component_fields/create',
-    component: CreateComponentFieldPage,
+    component: () => import('@/pages/Component/ComponentField/CreateComponentFieldPage.vue'),
     name: RouteNameComponentFieldEnum.CREATE_COMPONENT_FIELD,
     meta: { requiresAuth: true },
   },
