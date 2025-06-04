69fc292b98c1c98d178f5a903a6b9031d53fb39a
AntoineDly <antoine.delaunay333@gmail.com>
2025-06-03 18:17:38 -0700

    add pagination router link

---
+++ src/components/Pagination/PaginationRouterLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-link
+    :to="{
+      name: pagination.route,
+      query: { page: pagination.page, perPage: pagination.perPage },
+    }"
+  >
+    {{ pagination.page }}
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import { PaginationRouterLinkInterface } from '@/components/Pagination/pagination.interface'
+
+const pagination = defineProps<PaginationRouterLinkInterface>()
+</script>
